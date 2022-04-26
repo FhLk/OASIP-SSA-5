48983174b9b9979583ba887ae297c96d388e5b51
FhLk <paramat.fhlk@mail.kmutt.ac.th>
2022-04-26 17:02:40 +0700

    Update data test and TaskBoard

--- Week-1/OASIP-Client/src/App.vue
+++ Week-1/OASIP-Client/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import {ref} from 'vue';
-const category=ref({name:"",duration:0});
 const categories=ref([
     {name:"front-end",duration:60},
     {name:"back-end",duration:30},
@@ -9,61 +8,18 @@ const categories=ref([
     {name:"devops",duration:60},
     {name:"database",duration:60},
 ])
-const booking=ref({name:"",email:"",date:null,startTime:null ,category:category.value,note:""});
-const listdata=ref([
-    {
-        date:new Date("2022-4-22"),
-        startTime:"13:00",
-        duration: 60,
-        category:"Front-End Clinic",
-        booking:{name:"Paramat",email:"aaa@hotmail.com"},
-        note:"Wow"
-    },
-    {
-        date:new Date("2022,5,12"),
-        startTime:"10:30",
-        duration:30,
-        category:"Back-End Clinic",
-        booking:{name:"Michel",email:"bbb@hotmail.com"},
-        note:"wow"
-    },
-    {
-        date:new Date("2022,5,15"),
-        startTime:"09:00",
-        duration:30,
-        category:"Business Clinic",
-        booking:{name:"Jhon",email:"ccc@hotmail.com"},
-        note:"Wow"
-    },
-    {
-        date:new Date("2022,5,19"),
-        startTime:"10:00",
-        duration:60,
-        category:"Project-Manage Clinic",
-        booking:{name:"Bob",email:"ddd@hotmail.com"},
-        note:"Wow"
-    },
-    {
-        date:new Date("2022,5,22"),
-        startTime:"13:00",
-        duration:60,
-        category:"Database Clinic",
-        booking:{name:"Alice",email:"eee@hotmail.com"},
-        note:"Wow"
-    },
-    {
-        date:new Date("2022,5,30"),
-        startTime:"15:00",
-        duration:60,
-        category:"DevOps Clinic",
-        booking:{name:"Angle",email:"fff@hotmail.com"},
-        note:"Wow"
-    },
-]);
+const booking=ref({name:"",email:"",date:"",startTime:"" ,category:{name:"",duration:0},note:""});
+const listdata=ref([]);
 
-const Test=()=>{
-    console.log(booking.value.date)
-    console.log(booking.value.startTime)
+const Add=(event)=>{
+    if(event.email.includes('@')){
+        listdata.value.push(event)
+        booking.value={name:"",email:"",date:"",startTime:"" ,category:{name:"",duration:0},note:""};
+    }
+}
+
+const cancle=()=>{
+
 }
 
 </script>
@@ -73,14 +29,13 @@ const Test=()=>{
 <div>
     <h2>Event List</h2>
     <div>
-        <button>Add Event</button>
+        <button>Booking Event</button>
         <div>
             <p>Full Name: <input type="text" placeholder="Name..." v-model="booking.name"></p>
             <p>E-mail: <input type="email" placeholder="E-mail..." v-model="booking.email"></p>
             <p>Category: 
-            <!-- <form> -->
                 <ul v-for="(name,index) in categories" :key="index">
-                    <input type="radio" :id="name.name" :value="name" v-model="category">
+                    <input type="radio" :id="name.name" :value="name" v-model="booking.category">
                     - <label :for="name.name">{{name.name.toLocaleUpperCase()}}</label>
                 </ul>
                 <label>Date </label>: 
@@ -89,34 +44,36 @@ const Test=()=>{
                 <label> Start (Time) </label>: 
                 <input type="time" v-model="booking.startTime">
                 <br/>
-                <label>Duration (Minute): {{category.duration}}</label>
+                <label>Duration (Minute): {{booking.category.duration}}</label>
                 <br/>
                 <label>Note: </label>
                 <textarea rows="5" cols="50" v-model="booking.note"></textarea>
                 <div> 
-                    <button @click="Test">Sumit</button>
-                    <button @click="Test">Cancle</button>
+                    <button @click="Add(booking)" :disabled="
+                    booking.name==='' || booking.email==='' || !booking.email.includes('@') || booking.date===''
+                    || booking.startTime==='' || booking.category.name===''"
+                    >OK</button>
+                    <button @click="cancle">Cancle</button>
                 </div>
-            <!-- </form> -->
             </p>
         </div>
     </div>
     <div>
         <p>Sort By: | <a>Day</a> | <a>Upcoming</a> | <a>Past</a> | <a>Time</a> | </p>
     <ul>
-        <li v-for="(data,index) in listdata" :key="index">{{data.date.getMonth()}}/{{data.date.getDate()}}/{{data.date.getFullYear()}}
-            {{data.startTime}} ({{data.duration}} min.) {{data.category}}
-            {{data.booking.name}}
+        <li v-for="(data,index) in listdata" :key="index">{{data.date}}
+            {{data.startTime}} ({{data.category.duration}} min.) {{data.category.name.toLocaleUpperCase()}} Clinic
+            {{data.name}}
             <div>
             <button>Detail</button>
             <button>Delete</button>
             <div>
-                <p>Name: {{data.booking.name}}</p>
-                <p>E-mail: {{data.booking.email}}</p>
-                <p>Category: {{data.category}}</p>
+                <p>Name: {{data.name}}</p>
+                <p>E-mail: {{data.email}}</p>
+                <p>Category: {{data.category.name.toLocaleUpperCase()}}</p>
                 <p>Start Time: {{data.startTime}} PM.</p>
-                <p>Duration: {{data.duration}} min.</p>
-                <p>Date & Time: {{data.date.getMonth()}}/{{data.date.getDate()}}/{{data.date.getFullYear()}} {{data.startTime}} PM.</p>
+                <p>Duration: {{data.category.duration}} min.</p>
+                <p>Date & Time: {{data.date}} {{data.startTime}} PM.</p>
                 <p>Note: {{data.note}}</p>
                 <button>Edit</button>
             </div>
