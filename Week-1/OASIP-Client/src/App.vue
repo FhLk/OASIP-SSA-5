8d18da555e4a21ae1950506acd8713b10ede48ca
FhLk <paramat.fhlk@mail.kmutt.ac.th>
2022-04-28 15:09:52 +0700

    Update Client-side

--- Week-1/OASIP-Client/src/App.vue
+++ Week-1/OASIP-Client/src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
 import {ref} from 'vue';
+const isBooking=ref(false)
+const isDetail=ref(-1)
 const categories=ref([
     {name:"front-end",duration:60},
     {name:"back-end",duration:30},
@@ -8,20 +10,47 @@ const categories=ref([
     {name:"devops",duration:60},
     {name:"database",duration:60},
 ])
-const booking=ref({name:"",email:"",date:"",startTime:"" ,category:{name:"",duration:0},note:""});
-const listdata=ref([]);
+const booking=ref({name:"",email:"",group:"",date:"",startTime:"" ,category:{name:"",duration:0},note:""});
+const resetBooking=()=>{
+    booking.value={name:"",email:"",group:"",date:"",startTime:"" ,category:{name:"",duration:0},note:""};
+}
+const listdata=ref([
+    {
+        name:"Somchai Jaidee",
+        email:"somchai.jai@mail.kmutt.ac.th",
+        group:"OR-7",
+        date:"2022-05-23",
+        startTime:"13:30" ,
+        category:{name:"DevOps/Infra Clinic",duration:30},
+        note:""
+    },
+    {
+        name:"Somsri Rakdee",
+        email:"somsri.rak@mail.kmutt.ac.th",
+        group:"SJ-3",
+        date:"Project Management Clinic",
+        startTime:"2022-05-23 13:30" ,
+        category:{name:"DevOps/Infra Clinic",duration:30},
+        note:"ขอปรึกษาปัญหาเพื่อนไม่ช่วยงาน"
+    },
+    {
+        name:"สมเกียรติ ขยันเรียน",
+        email:"somkiat.kay@kmutt.ac.th",
+        group:"TT-4",
+        date:"2022-05-23",
+        startTime:"16:30" ,
+        category:{name:"Database Clinic",duration:30},
+        note:""
+    }
+]);
+
+
 
 const Add=(event)=>{
-    if(event.email.includes('@')){
-        listdata.value.push(event)
-        booking.value={name:"",email:"",date:"",startTime:"" ,category:{name:"",duration:0},note:""};
-    }
+    listdata.value.push(event)
+    resetBooking()
+    isBooking.value=false
 }
-
-const cancle=()=>{
-
-}
-
 </script>
  
 <template>
@@ -29,10 +58,11 @@ const cancle=()=>{
 <div>
     <h2>Event List</h2>
     <div>
-        <button>Booking Event</button>
-        <div>
+        <button @click="isBooking= isBooking ? false:true">Booking</button>
+        <div v-show="isBooking">
             <p>Full Name: <input type="text" placeholder="Name..." v-model="booking.name"></p>
             <p>E-mail: <input type="email" placeholder="E-mail..." v-model="booking.email"></p>
+            <p>Group: <input type="text" placeholder="Group" v-model="booking.group"/></p>
             <p>Category: 
                 <ul v-for="(name,index) in categories" :key="index">
                     <input type="radio" :id="name.name" :value="name" v-model="booking.category">
@@ -63,17 +93,17 @@ const cancle=()=>{
     <ul>
         <li v-for="(data,index) in listdata" :key="index">{{data.date}}
             {{data.startTime}} ({{data.category.duration}} min.) {{data.category.name.toLocaleUpperCase()}} Clinic
-            {{data.name}}
+            {{data.name}} ({{data.group}})
             <div>
-            <button>Detail</button>
+            <button @click="isDetail= isDetail===index ? -1:index">{{isDetail===index ? "Closed":"Detail"}}</button>
             <button>Delete</button>
-            <div>
+            <div v-show="isDetail===index">
                 <p>Name: {{data.name}}</p>
                 <p>E-mail: {{data.email}}</p>
                 <p>Category: {{data.category.name.toLocaleUpperCase()}}</p>
                 <p>Start Time: {{data.startTime}} PM.</p>
                 <p>Duration: {{data.category.duration}} min.</p>
-                <p>Date & Time: {{data.date}} {{data.startTime}} PM.</p>
+                <p>Date & Time: {{data.date}} {{data.startTime}}</p>
                 <p>Note: {{data.note}}</p>
                 <button>Edit</button>
             </div>
