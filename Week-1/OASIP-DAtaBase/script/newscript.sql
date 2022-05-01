f06fc1b0f8812121ab3527e1070eb0116f997e4c
FhLk <paramat.fhlk@mail.kmutt.ac.th>
2022-05-01 23:22:30 +0700

    Add Data Test

--- Week-1/OASIP-DAtaBase/script/newscript.sql
+++ Week-1/OASIP-DAtaBase/script/newscript.sql
@@ -29,6 +29,18 @@ CREATE TABLE IF NOT EXISTS `oasip_db`.`EventCategory` (
   UNIQUE INDEX `CategoryName_UNIQUE` (`CategoryName` ASC) VISIBLE)
 ENGINE = InnoDB;
 
+insert into `EventCategory` (`CategoryID`,`CategoryName`,`Description`,`Duration`) values 
+(1, 'Project Management Clinic',
+'ตารางนัดหมายนี้ใช้สำหรับนัดหมาย project
+management clinic ในวิชา INT221 integrated
+project I ให้นักศึกษาเตรียมเอกสารที่เกี่ยวข้องเพื่อแสEventBookingดง
+ระหว่างขอคำปรึกษา', 30 ),
+(2,'DevOps/Infra Clinic','Use this event category for DevOps/Infra clinic.',20),
+(3,'Database Clinic','ตารางนัดหมายนี้ใช้สำหรับนัดหมาย database clinic ใน
+วิชา INT221 integrated project I',15),
+(4,'Client-side Clinic','ตารางนัดหมายนี้ใช้สำหรับนัดหมาย client-side clinic ใน
+วิชา INT221 integrated project',30),
+(5,'Server-side Clinic',' ',30);
 
 -- -----------------------------------------------------
 -- Table `oasip_db`.`EventBooking`
@@ -49,6 +61,10 @@ CREATE TABLE IF NOT EXISTS `oasip_db`.`EventBooking` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+insert into `EventBooking` (`BookingID`,`BookingName`,`BookingEmail`,`CategoryID`,`StartTime`,`EventNote`) values 
+(1,'Somchai Jaidee (OR-7)','somchai.jai@mail.kmutt.ac.th',4,'2022-05-23 13:00',' '),
+(2,'Somsri Rakdee (SJ-3)','somsri.rak@mail.kmutt.ac.th',1,'2022-04-27 09:30',' ขอปรึกษาเรื่องเพพื่อนไม่ช่วยงาน '),
+(3,'สมเกียรติ ขยันเรียน กลุ่ม TT-4','somkiat.kay@kmutt.ac.th',3,'2022-05-23 16:30',' ');
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
--- Week-1/OASIP-Server/OASIP/src/main/java/sit/integrate/oasip/Controller/BookingController.java
+++ Week-1/OASIP-Server/OASIP/src/main/java/sit/integrate/oasip/Controller/BookingController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/bookings")
-@CrossOrigin(origins = "http://10.4.56.111:80",allowedHeaders = "*",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT})
+@CrossOrigin(origins = "http://10.4.56.111",allowedHeaders = "*",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT})
 public class BookingController {
     @Autowired
     private BookingService service;
