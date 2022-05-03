6b9d47eda04e01395fd68c22f5f05674ae05a3c9
FhLk <paramat.fhlk@mail.kmutt.ac.th>
2022-05-03 20:05:11 +0700

    update conmpose

--- Week-1/OASIP-Client/vite.config.js
+++ Week-1/OASIP-Client/vite.config.js
@@ -4,14 +4,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server:{
-    // proxy:{
-    //   '/api': {
-    //     target:'http://localhost:3000/',
-    //     changeOrigin:true,
-    //     secure:false,
-    //     // rewrite:(path) => path.replace(/^\/api/,'wow')
-    //   }
-    // }
-  }
 })
