6cba8d4dff6bcfcae311d46f913cd8db0c777528
FhLk <paramat.fhlk@mail.kmutt.ac.th>
2022-05-03 23:14:35 +0700

    Fix Cross origin

--- Week-1/OASIP-Client/vite.config.js
+++ Week-1/OASIP-Client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [vue()],
   server:{
     proxy:{
-      '/api':'http://oasip_SVR:8086'
+      '/api':'http://oasip_SVR:8086/'
     }
   }
 })
--- Week-1/OASIP-Server/OASIP/src/main/java/sit/integrate/oasip/ApplicationConfig.java
+++ Week-1/OASIP-Server/OASIP/src/main/java/sit/integrate/oasip/ApplicationConfig.java
@@ -4,10 +4,15 @@ package sit.integrate.oasip;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import sit.integrate.oasip.Utils.ListMapper;
 
 
 @Configuration
+@EnableWebMvc
 public class ApplicationConfig {
 
     @Bean
