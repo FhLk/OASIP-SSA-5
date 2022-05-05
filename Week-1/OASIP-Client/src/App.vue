7575ba9b6fb06e53833fa4025349aa3e64c708f7
FhLk <paramat.fhlk@mail.kmutt.ac.th>
2022-05-05 16:24:32 +0700

    UAT Config

--- Week-1/OASIP-Client/src/App.vue
+++ Week-1/OASIP-Client/src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
-import { onMounted, onBeforeMount, ref, onBeforeUpdate } from 'vue';
+import {onMounted,onBeforeMount,ref, onBeforeUpdate} from 'vue';
 import moment from "moment"
-const isBooking = ref(false)
-const isDetail = ref(-1)
-let DateFormat = "YYYY-MM-DD HH:mm A"
-const listdata = ref([]);
-const categories = ref([]);
-const booking = ref({
-    id: 0,
+const isBooking=ref(false)
+const isDetail=ref(-1)
+let DateFormat="YYYY-MM-DD HH:mm A"
+const getListBooking=ref([]);
+const getBooking=ref({});
+const categories=ref([]);
+const booking=ref({
     bookingName: "",
     bookingEmail: "",
     category: {
@@ -19,197 +19,152 @@ const booking = ref({
     startTime: "",
     eventNote: ""
 });
-const group = ref("")
-const bookingDate = ref("");
-const bookingTime = ref("")
-const resetBooking = () => {
-    booking.value = {
+const group=ref("")
+const bookingDate=ref("");
+const bookingTime=ref("")
+const resetBooking=()=>{
+    booking.value={
+    id: 0,
+    bookingName: "",
+    bookingEmail: "",
+    category: {
         id: 0,
-        bookingName: "",
-        bookingEmail: "",
-        category: {
-            id: 0,
-            categoryName: "",
-            description: "",
-            duration: 0
-        },
-        startTime: "",
-        eventNote: ""
-    };
+        categoryName: "",
+        description: "",
+        duration: 0
+    },
+    startTime: "",
+    eventNote: ""
+};
 }
-const showDetail = (id) => {
-    isDetail.value = isDetail.value === id - 1 ? -1 : id - 1
-}
-
-const getBookings = async () => {
-    const res = await fetch(`${import.meta.env.VITE_BASE_URL}/bookings`, {
+const showDetail = async (id)=>{
+    const res=await fetch(`${import.meta.env.VITE_BASE_URL}/bookings/${id}`,{
         method: 'GET'
     })
-    listdata.value = await res.json()
-    listdata.value.forEach((data) => {
-        data.startTime = moment(data.startTime).format(DateFormat)
-    })
+    getBooking.value=await res.json()
+    getBooking.value.startTime=moment(getBooking.value.startTime).format(DateFormat)
+    isDetail.value= isDetail.value===id ? -1:id
 }
 
-const getCategories = async () => {
-    const res = await fetch(`${import.meta.env.VITE_BASE_URL}/categories`, {
+const getBookings= async (startTime="startTime")=>{
+    const res=await fetch(`${import.meta.env.VITE_BASE_URL}/bookings?startTime=${startTime}`,{
         method: 'GET'
     })
-    categories.value = await res.json()
+    getListBooking.value=await res.json()
+    getListBooking.value.forEach((data)=>{
+        data.startTime=moment(data.startTime).format(DateFormat)
+    })
 }
 
-onMounted(async () => {
+
+const getCategories= async () =>{
+    const res=await fetch(`${import.meta.env.VITE_BASE_URL}/categories`,{
+        method: 'GET'
+    })
+    categories.value=await res.json()
+}
+
+onMounted(async ()=>{
     await getBookings()
     await getCategories()
 })
 
-const Add = async (event) => {
-    const res = await fetch(`${import.meta.env.VITE_BASE_URL}/bookings`, {
+const Add= async (event)=>{
+    const res=await fetch(`${import.meta.env.VITE_BASE_URL}/bookings`,{
         method: 'POST',
-        headers: {
+        headers:{
             'content-type': 'application/json'
         },
         body: JSON.stringify({
-            id: listdata.value.length + 1,
+            id:getListBooking.value.length+1,
             bookingName: event.bookingName + ` (${group.value})`,
             bookingEmail: event.bookingEmail,
             category: {
-                id: event.category.id,
+                id:event.category.id,
                 categoryName: event.category.categoryName,
                 description: event.category.description,
-                duration: event.category.duration
-            },
-            startTime: bookingDate.value + "T" + bookingTime.value + ":00Z",
-            eventNote: event.eventNote
+                duration: event.category.duration 
+                },
+            startTime:`${bookingDate.value}T${bookingTime.value}:00Z`,
+            eventNote: event.eventNote  
         })
     })
-    const newBooking = await res.json()
-    listdata.value.push(newBooking)
+    const newBooking =await res.json()
+    getListBooking.value.push(newBooking)
     resetBooking()
-    isBooking.value = false
+    isBooking.value=false
 }
 
-const isEditBooking = ref(false);
-const EditEvent = (event, index) => {
-    isBooking.value = false;
-    isEditBooking.value = isEditBooking.value ? false : true;
+const isEditBooking=ref(false);
+const EditEvent=(event,index)=>{
+    isBooking.value=false;
+    isEditBooking.value= isEditBooking.value ? false:true;
 }
 </script>
  
 <template>
-    <div class="bg h-screen font">
-        <h1 class="text-7xl  text-white mx-5 mb-4 ">OASIP</h1>
-        <div>
-            <div class="flex">
-                <h2 class="text-4xl  text-white ml-20 rounded-lg scd px-3">Schedule</h2>
-                <div
-                    class="rounded-lg p-2  flex-col  max-w-6xl mx-3 px-10 sm:px-10 lg:px-4 py-10 shadow-lg bg-neutral-100 space-y-2">
-                    <button @click="isBooking = isBooking ? false : true">Add Schedule</button>
-                    <div v-if="isBooking">
-                        <p>Full Name: <input type="text" placeholder="Name..." v-model="booking.bookingName"></p>
-                        <p>Group: <input type="text" placeholder="Group" v-model="group" class="mt-2"></p>
-                        <p>E-mail: <input type="email" placeholder="E-mail..." v-model="booking.bookingEmail"
-                                class="mt-2"></p>
-                        <select class="form-select appearance-none 
-                                    block
-                                    w-full
-                                    px-3
-                                    py-1.5
-                                    text-base
-                                    font-normal
-                                    text-gray-700
-                                    bg-white bg-clip-padding bg-no-repeat
-                                    border border-solid border-gray-300
-                                    rounded
-                                    transition
-                                    ease-in-out
-                                    m-0
-                                    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none mt-2"
-                            aria-label="Default select example">
-
-                            <option selected>Category</option>
-                            <option :for="index" value="1">Back-End clinic</option>l>
-                            <option :for="index" value="2">Database clinic</option>l>
-                            <option :for="index" value="3">Front-End clinic</option>l>
-                            <option :for="index" value="4">Project-Management</option>l>
-                            <option :for="index" value="5">DevOps-Infra clinic</option>l>
-                            <option :for="index" value="6">Business-Requirement clinic</option>l>
-
-                        </select>
-                        <!-- <ul v-for="(category, index) in categories" :key="index" >
-                            <input type="radio" :id="index" :value="category" v-model="booking.category" >
-                            - <label :for="index" >{{ category.categoryName }}</label>
-                        </ul> -->
-                        <label class="mt-2">Date </label>:
-                        <input type="date" v-model="bookingDate">
-                        <br />
-                        <label class="mt-2"> Start (Time) </label>:
-                        <input type="time" v-model="bookingTime">
-                        <br />
-                        <label class="mt-2">Duration (Minute): {{ booking.category.duration }}</label>
-                        <br />
-                        <label>Note: </label>
-                        <textarea rows="5" cols="50" v-model="booking.eventNote"></textarea>
-                        <div>
-                            <button @click="Add(booking)"
-                                class="rounded-lg p-2  flex-col  max-w-6xl mx-3 px-10 sm:px-10 lg:px-4 py-10 shadow-lg bg-green-600 space-y-2">OK</button>
-                            <button @click="cancle"
-                                class="rounded-lg p-2  flex-col  max-w-6xl mx-3 px-10 sm:px-10 lg:px-4 py-10 shadow-lg bg-red-600 space-y-2">Cancle</button>
-                        </div>
-                    </div>
+<h1>OASIP TEST</h1>
+<div>
+    <h2>Event List</h2>
+    <div>
+        <button @click="isBooking= isBooking ? false:true">Booking</button>
+        <div v-if="isBooking">
+            <p>Full Name: <input type="text" placeholder="Name..." v-model="booking.bookingName"></p>
+            <p>Group: <input type="text" placeholder="Group" v-model="group"/></p>
+            <p>E-mail: <input type="email" placeholder="E-mail..." v-model="booking.bookingEmail"></p>
+            <p>Category: 
+                <ul v-for="(category,index) in categories" :key="index">
+                    <input type="radio" :id="index" :value="category" v-model="booking.category">
+                    - <label :for="index">{{category.categoryName}}</label>
+                </ul>
+                <label>Date </label>: 
+                <input type="date" v-model="bookingDate">
+                <br/>
+                <label> Start (Time) </label>: 
+                <input type="time" v-model="bookingTime">
+                <br/>
+                <label>Duration (Minute): {{booking.category.duration}}</label>
+                <br/>
+                <label>Note: </label>
+                <textarea rows="5" cols="50" v-model="booking.eventNote"></textarea>
+                <div> 
+                    <button @click="Add(booking)">OK</button>
+                    <button @click="cancle">Cancle</button>
                 </div>
-            </div>
-            <div class="flex justify-end mr-12">
-                <button @click="delete" class="rounded-lg p-2 flex justify-end dl">delete</button>
-            </div>
-            <div class="rounded-lg  bg-white p-20 mt-2 mx-5 ">
-                <div v-if="listdata.length !== 0">
-                    <p>Sort By: | <a>Day</a> | <a>Upcoming</a> | <a>Past</a> | <a>Time</a> | </p>
-                    <ul>
-                        <li v-for="(data, index) in listdata" :key="index">{{ data.startTime }}
-                            ({{ data.category.duration }} min.) {{ data.category.categoryName.toLocaleUpperCase() }}
-                            Clinic
-                            {{ data.bookingName }}
-                            <div>
-                                <button @click="showDetail(index + 1)">{{ isDetail === index ? "Closed" : "Detail"
-                                }}</button>
-                                <button>Delete</button>
-                                <div v-if="isDetail === index">
-                                    <p>Name: {{ data.bookingName }}</p>
-                                    <p>E-mail: {{ data.bookingEmail }}</p>
-                                    <p>Category: {{ data.category.categoryName }}</p>
-                                    <p>Date & Time: {{ data.startTime }}</p>
-                                    <p>Duration: {{ data.category.duration }} min.</p>
-                                    <p>Start Time: {{ data.startTime.slice(10) }}</p>
-                                    <p>Note: {{ data.eventNote }}</p>
-                                    <button @click="EditEvent(data, index)">Edit</button>
-                                </div>
-                            </div>
-                            <br />
-                        </li>
-                    </ul>
-                </div>
-                <div v-else>
-                    <h2 class="text-1xl text-black flex justify-center">No Scheduled Events.</h2>
-                </div>
-            </div>
+            </p>
         </div>
     </div>
+    <div v-if="getListBooking
+.length!==0">
+        <p>Sort By: | <a>Day</a> | <a>Upcoming</a> | <a>Past</a> | <a>Time</a> | </p>
+    <ul>
+        <li v-for="(data,index) in getListBooking" :key="index">{{data.startTime}}
+            ({{data.category.duration}} min.) {{data.category.categoryName.toLocaleUpperCase()}}
+            {{data.bookingName}}
+            <div>
+            <button @click="showDetail(data.id)">{{isDetail===index ? "Closed":"Detail"}}</button>
+            <button>Delete</button>
+            <div v-if="isDetail===data.id">
+                <p>Name: {{getBooking.bookingName}}</p>
+                <p>E-mail: {{getBooking.bookingEmail}}</p>
+                <p>Category: {{getBooking.category.categoryName}}</p>
+                <p>Date & Time: {{getBooking.startTime}}</p>
+                <p>Duration: {{getBooking.category.duration}} min.</p>
+                <p>Start Time: {{getBooking.startTime.slice(10)}}</p>
+                <p>Note: {{getBooking.eventNote}}</p>
+                <button @click="EditEvent(data,index)">Edit</button>
+            </div>
+            </div>
+            <br/>
+        </li>
+    </ul>
+    </div> 
+    <div v-else>
+        <h2>No Scheduled Events.</h2>
+    </div>
+</div>
 </template>
  
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Titan+One&display=swap');
 
-.bg {
-    background-color: rgb(25, 25, 112);
-}
-.font {
-    font-family: 'Titan One', cursive;
-}
-.dl{
-    background-color: rgb(215, 45, 45);
-}
-.scd{
-    background-color: rgb(86, 165, 236);
-}
 </style>
--- Week-1/OASIP-DataBase/script/datascript.sql
+++ Week-1/OASIP-DataBase/script/datascript.sql
@@ -1,3 +1,6 @@
+select * from EventCategory;
+select * from EventBooking;
+
 insert into `EventCategory` (`CategoryID`,`CategoryName`,`Description`,`Duration`) values 
 (1, 'Project Management Clinic',
 'ตารางนัดหมายนี้ใช้สำหรับนัดหมาย project
@@ -12,6 +15,13 @@ project I ให้นักศึกษาเตรียมเอกสาร�
 (5,'Server-side Clinic',' ',30);
 
 insert into `EventBooking` (`BookingID`,`BookingName`,`BookingEmail`,`CategoryID`,`StartTime`,`EventNote`) values 
-(1,'Somchai Jaidee (OR-7)','somchai.jai@mail.kmutt.ac.th',4,'2022-05-23 13:00',' '),
+(1,'Somchai Jaidee (OR-7)','somchai.jai@mail.kmutt.ac.th',2,'2022-05-23 13:30',' '),
 (2,'Somsri Rakdee (SJ-3)','somsri.rak@mail.kmutt.ac.th',1,'2022-04-27 09:30',' ขอปรึกษาเรื่องเพพื่อนไม่ช่วยงาน '),
 (3,'สมเกียรติ ขยันเรียน กลุ่ม TT-4','somkiat.kay@kmutt.ac.th',3,'2022-05-23 16:30',' ');
+
+delete from EventBooking where BookingID in (4,5,6);
+
+delete from EventBooking where BookingID in (1,2,3);
+
+
+
