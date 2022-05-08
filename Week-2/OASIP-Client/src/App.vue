1df7eb2cc9d075140e7fe90985ddcb61270a2332
Name63130500043 <noteatomic678@gmail.com>
2022-05-09 00:45:22 +0700

    tailwind ep1

--- Week-2/OASIP-Client/src/App.vue
+++ Week-2/OASIP-Client/src/App.vue
@@ -1,56 +1,56 @@
 <script setup>
-import {onMounted,onBeforeMount,ref, onBeforeUpdate, onUpdated, computed} from 'vue';
+import { onMounted, onBeforeMount, ref, onBeforeUpdate, onUpdated, computed } from 'vue';
 import moment from "moment"
 import List from "./components/List.vue"
 import Create from './components/Create.vue';
-let DateFormat="YYYY-MM-DD HH:mm"
-const getListBooking=ref([])
-const getListCategories=ref([]);
+let DateFormat = "YYYY-MM-DD HH:mm"
+const getListBooking = ref([])
+const getListCategories = ref([]);
 
-const getCategories= async () =>{
-    const res=await fetch(`${import.meta.env.VITE_BASE_URL}/categories`,{
+const getCategories = async () => {
+    const res = await fetch(`${import.meta.env.VITE_BASE_URL}/categories`, {
         method: 'GET'
     })
-    getListCategories.value=await res.json()
+    getListCategories.value = await res.json()
 }
 
-const getBookings= async ()=>{
-    const res=await fetch(`${import.meta.env.VITE_BASE_URL}/bookings`,{
+const getBookings = async () => {
+    const res = await fetch(`${import.meta.env.VITE_BASE_URL}/bookings`, {
         method: 'GET'
     })
-    getListBooking.value=await res.json()
-    getListBooking.value.forEach((data)=>{
-        data.startTime=moment(data.startTime).utcOffset(0).format(DateFormat)
+    getListBooking.value = await res.json()
+    getListBooking.value.forEach((data) => {
+        data.startTime = moment(data.startTime).utcOffset(0).format(DateFormat)
     })
-    getListBooking.value.sort((a,b)=>{
+    getListBooking.value.sort((a, b) => {
         return new Date(b.startTime) - new Date(a.startTime)
     })
 }
 
-onBeforeMount(async ()=>{
+onBeforeMount(async () => {
     await getBookings()
     await getCategories()
 })
 
-const createBooking= async (booking)=>{
-    const Auto_Increment =(increment)=>{
-        getListBooking.value.forEach((data)=>{
-            if(increment===data.id){
-                increment=data.id+1
+const createBooking = async (booking) => {
+    const Auto_Increment = (increment) => {
+        getListBooking.value.forEach((data) => {
+            if (increment === data.id) {
+                increment = data.id + 1
             }
-            else{
-                increment=getListBooking.value.length+1
+            else {
+                increment = getListBooking.value.length + 1
             }
         })
         return increment
     }
-    const res=await fetch(`${import.meta.env.VITE_BASE_URL}/bookings`,{
+    const res = await fetch(`${import.meta.env.VITE_BASE_URL}/bookings`, {
         method: 'POST',
-        headers:{
+        headers: {
             'content-type': 'application/json'
         },
         body: JSON.stringify({
-            id: Auto_Increment(getListBooking.value.length+1),
+            id: Auto_Increment(getListBooking.value.length + 1),
             bookingName: booking.bookingName + ` (${booking.group})`,
             bookingEmail: booking.bookingEmail,
             category: {
@@ -59,41 +59,65 @@ const createBooking= async (booking)=>{
                 description: booking.category.description,
                 duration: booking.category.duration
             },
-            startTime:`${booking.Date}T${booking.Time}:00Z`,
-            eventNote: booking.eventNote  
+            startTime: `${booking.Date}T${booking.Time}:00Z`,
+            eventNote: booking.eventNote
         })
     })
-    if(res.status===201){
+    if (res.status === 201) {
         await getBookings()
     }
 }
-const deleteBooking= async (booking)=>{
-    if(confirm("Do you want cancel this Booking ?")){
+const deleteBooking = async (booking) => {
+    if (confirm("Do you want cancel this Booking ?")) {
         const res = await fetch(`${import.meta.env.VITE_BASE_URL}/bookings/${booking.id}`, {
             method: 'DELETE'
         })
-        if(res.status===200){
-            getListBooking.value =getListBooking.value.filter(b => b!==booking)
+        if (res.status === 200) {
+            getListBooking.value = getListBooking.value.filter(b => b !== booking)
         }
     }
 }
-const saveBooking=(updateBooking)=>{
-    getListBooking.value=getListBooking.value.map((booking)=>{
-        return booking.id===updateBooking.id ? {...booking,updateBooking}:booking
+const saveBooking = (updateBooking) => {
+    getListBooking.value = getListBooking.value.map((booking) => {
+        return booking.id === updateBooking.id ? { ...booking, updateBooking } : booking
     })
     location.reload()
 }
 </script>
  
 <template>
-<h1>OASIP TEST</h1>
-<div>
-    <h2>booking List</h2>
-    <Create @add="createBooking" :getCategories="getListCategories" :getListBooking="getListBooking"/>
-    <List @save="saveBooking" @delete="deleteBooking" :getListBooking="getListBooking"/>
-</div>
+    <div class="bg h-screen font">
+        <h1 class="text-7xl text-white pl-10 pt-2 pb-2 mb-4 oa">OASIP TEST</h1>
+        
+            <div class="scd rounded-lg w-48 ml-10 p-2 flex-none">
+                <h2 class="text-4xl text-white flex justify-center">Schedule</h2>
+            </div>
+            <div class="bg">
+                <Create @add="createBooking" :getCategories="getListCategories" :getListBooking="getListBooking" />
+            </div>
+       
+        <div class="bg">
+            <List @save="saveBooking" @delete="deleteBooking" :getListBooking="getListBooking" />
+        </div>
+    </div>
 </template>
  
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Titan+One&display=swap');
 
+.bg {
+    background-color: rgb(25, 25, 112);
+}
+
+.font {
+    font-family: 'Titan One', cursive;
+}
+
+.oa {
+    background-color: rgb(66, 155, 219);
+}
+
+.scd {
+    background-color: rgb(86, 165, 236);
+}
 </style>
--- Week-2/OASIP-Client/src/components/Create.vue
+++ Week-2/OASIP-Client/src/components/Create.vue
@@ -1,38 +1,38 @@
 <script setup>
 import { onBeforeMount, ref } from 'vue';
-const isBooking=ref(false)
+const isBooking = ref(false)
 
 defineEmits(['add'])
 
-const props=defineProps({
-  getCategories: {
-    type: Array,
-    require: true
-  },
-  getListBooking:{
-      type: Array,
-      require:true
-  }
+const props = defineProps({
+    getCategories: {
+        type: Array,
+        require: true
+    },
+    getListBooking: {
+        type: Array,
+        require: true
+    }
 })
 
-const newbooking=ref({
+const newbooking = ref({
     bookingName: "",
-    group:"",
+    group: "",
     bookingEmail: "",
-    Date:"",
-    Time:"",
+    Date: "",
+    Time: "",
     category: {},
     eventNote: "",
 });
 
-const reset=()=>{
-    isBooking.value=false
-    newbooking.value={
+const reset = () => {
+    isBooking.value = false
+    newbooking.value = {
         bookingName: "",
-        group:"",
+        group: "",
         bookingEmail: "",
-        Date:"",
-        Time:"",
+        Date: "",
+        Time: "",
         category: {},
         eventNote: "",
     }
@@ -42,35 +42,35 @@ const reset=()=>{
  
 <template>
 <div>
-    <div>
-        <button @click="isBooking= isBooking ? false:true">Booking</button>
-        <div v-if="isBooking">
-            <p>Full Name: <input type="text" placeholder="Name..." v-model="newbooking.bookingName"></p>
-            <p>Group: <input type="text" placeholder="Group" v-model="newbooking.group"/></p>
-            <p>E-mail: <input type="email" placeholder="E-mail..." v-model="newbooking.bookingEmail"></p>
-            <p>Category: 
-                <ul v-for="(category,index) in getCategories " :key="index">
-                    <input type="radio" :id="index" :value="category" v-model="newbooking.category">
-                    - <label :for="index">{{category.categoryName}}</label>
-                </ul>
-                <label>Date </label>: 
-                <input type="date" v-model="newbooking.Date">
-                <br/>
-                <label> Start (Time) </label>: 
-                <input type="time" v-model="newbooking.Time">
-                <br/>
-                <label>Duration (Minute): {{newbooking.category.duration}}</label>
-                <br/>
-                <label>Note: </label>
-                <textarea rows="5" cols="50" v-model="newbooking.eventNote"></textarea>
-                <div> 
-                    <button @click="$emit('add',newbooking),reset()">OK</button>
-                    <button @click="reset">Cancle</button>
-                </div>
-            </p>
-        </div>
+    <div class="">
+        <button @click="isBooking = isBooking ? false : true">Add Booking</button>
     </div>
-</div>
+    <div v-if="isBooking">
+        <p>Full Name: <input type="text" placeholder="Name..." v-model="newbooking.bookingName"></p>
+        <p>Group: <input type="text" placeholder="Group" v-model="newbooking.group" /></p>
+        <p>E-mail: <input type="email" placeholder="E-mail..." v-model="newbooking.bookingEmail"></p>
+        <p>Category:
+        <ul v-for="(category, index) in getCategories " :key="index">
+            <input type="radio" :id="index" :value="category" v-model="newbooking.category">
+            - <label :for="index">{{ category.categoryName }}</label>
+        </ul>
+        <label>Date </label>:
+        <input type="date" v-model="newbooking.Date">
+        <br />
+        <label> Start (Time) </label>:
+        <input type="time" v-model="newbooking.Time">
+        <br />
+        <label>Duration (Minute): {{ newbooking.category.duration }}</label>
+        <br />
+        <label>Note: </label>
+        <textarea rows="5" cols="50" v-model="newbooking.eventNote"></textarea>
+        <div>
+            <button @click="$emit('add', newbooking), reset()">OK</button>
+            <button @click="reset">Cancle</button>
+        </div>
+        </p>
+    </div>
+</div>        
 </template>
  
 <style scoped>
