6f547a230f16896c7270a8e497daf042b5e82cf9
FhLk <paramat.fhlk@mail.kmutt.ac.th>
2022-05-19 19:15:28 +0700

    Change url on Front-end

--- Week-3/OASIP-Client/vite.config.js
+++ Week-3/OASIP-Client/vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'/ssa5/',
+  // base:'/ssa5/',
   server:{
     proxy:{
       '/api':'http://oasipSVR:8080/api'
